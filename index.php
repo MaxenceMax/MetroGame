1de4218d9446d8e6c73ec72d02afc24cbb350874
Max <max@mbp-de-max.parc.univ-paris1.fr>
2014-03-31 15:14:04 +0200

    Push du projet

---
+++ index.php
@@ -0,0 +1,9 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" type="text/css" media="screen" href="style/style.css">
+</head>
+<body>
+    <div class="divSimple">
+    </div>
+</body>
+</html>
