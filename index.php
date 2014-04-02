0fea34d5770127ac6ea5a05a597063c771052d53
Max <max@MacBook-Pro-de-Max.local>
2014-04-02 16:00:52 +0200

    ajout du style et mise en place du style de l'accueil

--- index.php
+++ index.php
@@ -3,7 +3,32 @@
 <link rel="stylesheet" type="text/css" media="screen" href="style/style.css">
 </head>
 <body>
-    <div class="divSimple">
+    <div id="Global">
+        <div id="Logo">
+        <a href="/"></a>
+        </div>
+        <div id="ColonneGauche">
+            <div id="Home">
+                <a title="Retour Accueil" href="/">Accueil</a>
+            </div>
+            <div id="Menu">
+                <ul class="navigation"></ul>
+            </div>
+        </div>   
+            <div id="Contenu">
+            </div>
+        <div id="Ombre"></div>
     </div>
+    <div id="BarreInterne">
+    <div id="TexteAccueil">
+        <h2 style="border: none">
+	Bienvenue sur le site d'Omecad</h2><p>
+	Avant d'ouvrir nos pages spécifiques, retenez qu' Omecad :<br><br>
+	C'est un état d'esprit, des méthodes de travail, un partage permanent d'expériences et de compétences, un système qualité maitrisé pour " UN OBJECTIF à Atteindre" : LE VÔTRE.<br><br>
+	C'est une capacité d'écoute, d'analyse, d'échange pour intégrer votre savoir faire et y apporter notre regard pragmatique et nos propres expériences pour un meilleur résultat.<br><br>
+	C'est un lien fort sur les thématiques de la mécanique, de la conception à la réalisation avec les outils numériques performants propre à chaque domaine.<br><br>
+	Enfin le mot éthique a un sens chez Omecad tant dans le respect de nos engagements, la manière d'atteindre nos objectifs communs que pour la protection de vos données et savoir-faire.</p>
+    </div>
+</div>
 </body>
 </html>
