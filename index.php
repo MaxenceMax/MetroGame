9543f91a4d49d3c7bc3c458fde10d6130a536708
Max <max@pc2.home>
2014-04-04 15:44:07 +0200

    derniere modofication maxence

--- index.php
+++ index.php
@@ -20,15 +20,11 @@
         <div id="Ombre"></div>
     </div>
     <div id="BarreInterne">
-    <div id="TexteAccueil">
-        <h2 style="border: none">
-	Bienvenue sur le site d'Omecad</h2><p>
-	Avant d'ouvrir nos pages spécifiques, retenez qu' Omecad :<br><br>
-	C'est un état d'esprit, des méthodes de travail, un partage permanent d'expériences et de compétences, un système qualité maitrisé pour " UN OBJECTIF à Atteindre" : LE VÔTRE.<br><br>
-	C'est une capacité d'écoute, d'analyse, d'échange pour intégrer votre savoir faire et y apporter notre regard pragmatique et nos propres expériences pour un meilleur résultat.<br><br>
-	C'est un lien fort sur les thématiques de la mécanique, de la conception à la réalisation avec les outils numériques performants propre à chaque domaine.<br><br>
-	Enfin le mot éthique a un sens chez Omecad tant dans le respect de nos engagements, la manière d'atteindre nos objectifs communs que pour la protection de vos données et savoir-faire.</p>
+        <div id="TexteAccueil">
+            <h2 style="border: none">
+        </div>
     </div>
-</div>
+    <div id="Footer">
+    </div>
 </body>
 </html>
